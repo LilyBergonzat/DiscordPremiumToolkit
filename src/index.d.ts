c05d9655f2321bee8cdf77a57b31b90c94bb75a3
Lily Wonhalf <lilywonhalf@gmail.com>
2023-08-09 10:40:16 +0200

    Deleted useless files

--- src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { BootstrapOptions } from '#root/setup/Bootstrap';
-import type { Interaction } from 'discord.js';
-export declare function setupDiscordPremiumKit(data: BootstrapOptions): void;
-export declare function replyWithPremiumCTA(interaction: Interaction): Promise<void>;
-//# sourceMappingURL=index.d.ts.map
