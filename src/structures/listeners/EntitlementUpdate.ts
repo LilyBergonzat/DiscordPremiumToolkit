aa7be5ea11d9e6e1ff0984b3b9a13b1a7b1ef5e7
Lily Wonhalf <lilywonhalf@gmail.com>
2023-08-14 02:24:47 +0200

    Fixed class name

--- src/structures/listeners/EntitlementUpdate.ts
+++ src/structures/listeners/EntitlementUpdate.ts
@@ -1,7 +1,7 @@
 import type Listener from '../Listener';
 import type { Entitlement } from '#structures/Entitlement';
 
-export default class EntitlementCreate implements Listener {
+export default class EntitlementUpdate implements Listener {
     public run(entitlement: Entitlement): void {
         entitlement.client.application?.entitlements.add(entitlement);
     }
